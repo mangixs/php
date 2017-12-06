296e3fbcfb0ba6b8c553ff03e39f31c931b631b1
mangixs <fnhdcwy@hotmail.com>
2017-12-06 10:57:26 +0800

    test

--- git.php
+++ git.php
@@ -1,4 +1,5 @@
 <?php
 #https://www.liaoxuefeng.com/wiki/0013739516305929606dd18361248578c67b8067c8c017b000
 #http://www.yiibai.com/git/git_push.html
-#这里测试git的分支
+#这里测试git的分支、、
+#git push orign mareer
