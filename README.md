8cbe5829b1a9b6f936ae49651f9c918d4c8f16b5
mangixs <fnhdcwy@hotmail.com>
2018-06-29 21:58:00 +0800

    2018-06-29-21:57

--- README.md
+++ README.md
@@ -2,4 +2,10 @@
 在php.ini中
 disble_functions = phpinfo
 重启apache就能实现屏蔽phpinfo（）；函数了。
-在这里测试git
+
+yii 获取查询语句
+$commandQuery = clone $query;
+echo $commandQuery->createCommand()->getRawSql();
+
+tp 获取查询语句
+getLastSql()
--- common/common.php
+++ common/common.php
@@ -629,3 +629,49 @@ function getUrlQuery($array_query) {
     $params = implode('&', $tmp);
     return $params;
 }
+//无限分类
+function getTree($data, $pId = 1) {
+    $tree = '';
+    foreach ($data as $k => $v) {
+        if ($v['parent_id'] == $pId) {
+            $v['list'] = $this->getTree($data, $v['id']);
+            $tree[]    = $v;
+        }
+    }
+    return $tree;
+}
+//无限分类转html
+function toHtml($tree) {
+    $html = '';
+    foreach ($tree as $t) {
+        if ($t['list'] == '') {
+            $html .= "<li>{$t['name']}</li>";
+        } else {
+            $html .= "<li>" . $t['name'];
+            $html .= procHtml($t['parent_id']);
+            $html = $html . "</li>";
+        }
+    }
+    return $html ? '<ul>' . $html . '</ul>' : $html;
+}
+/**
+ * 根据起点坐标和终点坐标测距离
+ * @param  [array]   $from  [起点坐标(经纬度),例如:array(118.012951,36.810024)]
+ * @param  [array]   $to    [终点坐标(经纬度)]
+ * @param  [bool]    $km        是否以公里为单位 false:米 true:公里(千米)
+ * @param  [int]     $decimal   精度 保留小数位数
+ * @return [string]  距离数值
+ */
+function get_distance($from, $to, $km = true, $decimal = 2) {
+    sort($from);
+    sort($to);
+    $EARTH_RADIUS = 6370.996; // 地球半径系数
+
+    $distance = $EARTH_RADIUS * 2 * asin(sqrt(pow(sin(($from[0] * pi() / 180 - $to[0] * pi() / 180) / 2), 2) + cos($from[0] * pi() / 180) * cos($to[0] * pi() / 180) * pow(sin(($from[1] * pi() / 180 - $to[1] * pi() / 180) / 2), 2))) * 1000;
+
+    if ($km) {
+        $distance = $distance / 1000;
+    }
+
+    return round($distance, $decimal);
+}
